4021f6818d44c278f86760fa3b8027bf8930df28
dharshan047 <dharshan0747@gmail.com>
2025-12-15 21:16:03 +0530

    update global types and declarations

---
+++ globals.d.ts
@@ -0,0 +1,11 @@
+declare module "*.css";
+
+declare global {
+  interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    gtag: (...args: any[]) => void;
+  }
+}
+
+// This is required so the file becomes a module.
+export {};
